9b848697d8a2f9b499c87bc932a66ae516c1d152
KelvinFerdinand <kelvinferdinand1@gmail.com>
2023-06-15 23:08:49 +0700

    Sign up dan login fixing

--- lib/main.dart
+++ lib/main.dart
@@ -231,17 +231,20 @@ class _HomePageState extends State<HomePage> {
           //     ),
 
           Container(
-            alignment: Alignment.center,
+            alignment: Alignment.center, 
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0XFF6D6E4C)),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0)), backgroundColor: Color(0XFF6D6E4C)),
               onPressed: () {},
               child: const Padding(
-                padding: EdgeInsets.fromLTRB(75, 5, 75, 5),
+                padding: EdgeInsets.fromLTRB(128, 5, 128, 5),
                 child: Text(
-                  "Sign Up",
+                  "SIGN UP",
                   style: TextStyle(
-                    color: Colors.black,
+                  fontFamily: 'Lato',
+                  fontWeight: FontWeight.w900,
+                  color: Color(0xffFFFFFF),
                   ),
                 ),
               ),
@@ -272,7 +275,9 @@ class _HomePageState extends State<HomePage> {
                     alignment: Alignment.center,
                   ),
                   TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                      },
                       child: const Text("Already Have an Account? Login Here"))
                 ],
               )),
@@ -302,100 +307,102 @@ class LoginPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
+    body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           const SizedBox(height: 40),
-          const Text(
-            'LOG IN!',
-            style: TextStyle(
-                fontFamily: 'Lato',
-                fontSize: 30,
-                fontWeight: FontWeight.w900,
-                color: Color(0XFF6D6E4C)),
-          ),
-          const SizedBox(height: 1),
-          const Text(
-            'Welcome Back',
-            style: TextStyle(
-              fontFamily: 'Raleway',
-              fontSize: 15,
-              fontWeight: FontWeight.w800,
-              color: Color(0XFF000000),
+          const Padding(
+            padding: EdgeInsets.only(left: 18),
+            child: Text(
+              'LOG IN!',
+              style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: Color(0XFF6D6E4C)),
             ),
           ),
           const SizedBox(height: 1),
-          const Text(
-            'Hello there Log in to continue...',
-            style: TextStyle(
-              fontFamily: 'RalewayReg',
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: Color(0xff000000),
+          const Padding(
+            padding: EdgeInsets.only(left: 18),
+            child: Text(
+              'Welcome Back',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 15,
+                fontWeight: FontWeight.w800,
+                color: Color(0XFF000000),
+              ),
             ),
           ),
-          const TextField(
-            decoration: InputDecoration(),
+          const SizedBox(height: 5),
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              'Hello there Log In to continue...',
+              style: TextStyle(
+                fontFamily: 'RalewayReg',
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff000000),
+              ),
+            ),
           ),
-          Container(
-              margin: const EdgeInsets.fromLTRB(15, 25, 25, 15),
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintStyle: TextStyle(color: Colors.black),
-                    labelText: 'Enter Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    contentPadding: EdgeInsets.fromLTRB(20, 5, 20, 5)),
-                obscureText: true,
-              )),
-          Container(
-              margin: const EdgeInsets.fromLTRB(15, 10, 25, 15),
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintStyle: TextStyle(color: Colors.black),
-                    labelText: 'Enter Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    contentPadding: EdgeInsets.fromLTRB(20, 5, 20, 5)),
-                obscureText: true,
-              )),
-          Container(
-            child: TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(const Color(0XFF6D6E4C)),
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(const Color(0XFF6D6E4C)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
-                    ),
+        const SizedBox(height: 25),
+        Container(
+            margin: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.black),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.fromLTRB(20, 5, 20, 5)),
+              obscureText: true,
+            )),
+        Container(
+            margin: const EdgeInsets.fromLTRB(15, 10, 15, 15),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.black),
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.fromLTRB(20, 5, 20, 5)),
+              obscureText: true,
+            )),
+        Container(
+            alignment: Alignment.center, 
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0)), backgroundColor: Color(0XFF6D6E4C)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(132, 5, 132, 5),
+                child: Text(
+                  "LOG IN",
+                  style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontWeight: FontWeight.w900,
+                  color: Color(0xffFFFFFF),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-                child: const Column(
-                  children: [
-                    Text(
-                        '                                                 LOGIN                                                  ',
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xffFFFFFF),
-                        ))
-                  ],
-                )),
-          )
+              ),
+            ),
+          ),
         ],
       ),
     );
