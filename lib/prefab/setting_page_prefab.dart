d61433520ea37aa4b0aa2af849e2f4210181c31b
NegaNe <manfireburst@gmail.com>
2023-06-23 16:20:22 +0700

    Settings Fixed

--- lib/prefab/setting_page_prefab.dart
+++ lib/prefab/setting_page_prefab.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class BoxReuseable extends StatelessWidget {
-  BoxReuseable({this.text});
-
+  BoxReuseable({this.text, required this.container});
+  final Container container;
   final Text? text;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.amber,
-      body: Text(''),
+    return Container(
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(color: Colors.amber),
+      margin: EdgeInsets.all(2),
+      padding: EdgeInsets.only(left: 10),
+      child: container,
+
+      // backgroundColor: Colors.amber,
+      // body: text,
     );
   }
 }
--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -17,23 +17,35 @@ class Setting extends StatelessWidget {
               child: Column(
             children: [
               SizedBox(
-                height: 25,
+                height: 45,
               ),
               Container(
                 alignment: Alignment.centerLeft,
                 padding: EdgeInsets.only(left: 10),
-                child: Text('Account Settings'),
+                child: Text(
+                  'Account Settings',
+                ),
+              ),
+              Expanded(
+                child: BoxReuseable(
+                  container: Container(
+                    child: Text('Privacy & Safety'),
+                  ),
+                ),
               ),
               Expanded(
                 child: BoxReuseable(
                   text: Text('$_contents'),
+                  container: Container(
+                    child: Text('My Address'),
+                  ),
                 ),
               ),
               Expanded(
-                child: BoxReuseable(),
-              ),
-              Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Credentials')),
+                ),
               ),
               Container(
                 alignment: Alignment.centerLeft,
@@ -41,36 +53,70 @@ class Setting extends StatelessWidget {
                 child: Text('Settings'),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Chat Settings')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Notification Settings')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Privacy Settings')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Language')),
+                ),
               ),
               Container(
                 alignment: Alignment.centerLeft,
                 padding: EdgeInsets.only(left: 10),
-                child: Text('Account Settings'),
+                child: Text('Support'),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Help')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Rules')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('FAQ')),
+                ),
               ),
               Expanded(
-                child: BoxReuseable(),
+                child: BoxReuseable(
+                  text: Text('$_contents'),
+                  container: Container(child: Text('Terms and Condition')),
+                ),
               ),
               SizedBox(
-                height: 75,
+                height: 25,
+              ),
+              SizedBox(
+                child: Container(
+                  decoration: BoxDecoration(color: Color(0XFF6D6E4C)),
+                  child: TextButton(onPressed: () {}, child: Text("Logout")),
+                  width: 150,
+                ),
+              ),
+              SizedBox(
+                height: 25,
               )
             ],
           ))
