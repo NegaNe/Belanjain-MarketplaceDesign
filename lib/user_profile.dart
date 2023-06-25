fe052b97aea12457a693706b720e3bc19536f68e
Surya Halim <suryahalim788@gmail.com>
2023-06-25 16:03:07 +0700

    Update user_profile.dart

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -4,7 +4,7 @@ import 'package:marketplace/prefab/setting_page_prefab.dart';
 
 class UserProfile extends StatelessWidget {
   const UserProfile({super.key});
-  final String _contents = 'MyInsides';
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +25,67 @@ class UserProfile extends StatelessWidget {
                 padding: EdgeInsets.only(left: 10),
                 child: Text(
                   'My Purchase',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Oswald',
+                    fontWeight: FontWeight.w700,
+                    // color: Color(0XFFFFFFF),
+                  )
+                ), 
+              ),
+              Expanded(
+                child: BoxReuseable(
+                  container: Container(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      'To Pay'
+                    ),
+                  )
+                )
+              ),
+              Expanded(
+                child: BoxReuseable(
+                  container: Container(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      'To Ship'
+                    ),
+                  )
+                )
+              ),
+              Expanded(
+                child: BoxReuseable(
+                  container: Container(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      'To Receive'
+                    ),
+                  )
+                )
+              ),
+              Expanded(
+                child: BoxReuseable(
+                  container: Container(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      'To Rate'
+                    ),
+                  )
+                )
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                child: Container(
+                  decoration: BoxDecoration(color: Color(0XFF6D6E4C)),
+                  child: TextButton(onPressed: () {}, child: Text("Logout")),
+                  width: 150,
                 ),
               ),
+              SizedBox(
+                height: 25,
+              )
             ],
           ))
         ],
