c26b6961cdb8b88e99a14be1ed553e63581ad8eb
NegaNe <manfireburst@gmail.com>
2023-06-29 20:20:25 +0700

    4 files buat pages yg masih kosong

---
+++ lib/chat_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class chatPage extends StatelessWidget {
+  const chatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
