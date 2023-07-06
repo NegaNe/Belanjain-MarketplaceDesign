aa8de2aae506fdabb7b004e35da25fbcd7c2e069
NegaNe <manfireburst@gmail.com>
2023-07-07 01:17:33 +0700

    fix-border pp

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -52,38 +52,41 @@ class UserProfile extends StatelessWidget {
             child: Row(
               children: [
                 Container(
-                  margin: EdgeInsets.fromLTRB(40,3, 0, 0),
+                  margin: EdgeInsets.fromLTRB(40, 3, 0, 0),
                   height: 80.0,
                   width: 80.0,
                   decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.white),
-                      right: BorderSide(color: Colors.white),
-                      ),
-                    borderRadius: BorderRadius.circular(50), 
-                    image: DecorationImage(
-                      image: NetworkImage('https://picsum.photos/seed/picsum/200/200'),
+                    border: Border.all(width: 1.5, color: Colors.white),
+                    borderRadius: BorderRadius.circular(50),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://picsum.photos/seed/picsum/200/200'),
                       fit: BoxFit.cover,
                     ),
                   ),
-              ),
+                ),
                 const Row(
                   children: [
-                    Padding(padding: EdgeInsets.fromLTRB(25, 0, 0, 0),),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                    ),
                     Text.rich(TextSpan(children: [
                       TextSpan(
-                          text: 'Petter Official Store\n',
-                          style: TextStyle(
-                            fontFamily:'OswaldReg',
-                            fontSize: 20, color: Colors.white,
-                            ),
-                            ),
+                        text: 'Petter Official Store\n',
+                        style: TextStyle(
+                          fontFamily: 'OswaldReg',
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
                       TextSpan(
-                          text: 'Followers: 11k | Following: 10k',
-                          style: TextStyle(
-                            fontFamily: 'OswaldReg',
-                            fontSize: 10, color: Colors.white,
-                          ),),
+                        text: 'Followers: 11k | Following: 10k',
+                        style: TextStyle(
+                          fontFamily: 'OswaldReg',
+                          fontSize: 10,
+                          color: Colors.white,
+                        ),
+                      ),
                     ]))
                   ],
                 )
@@ -108,12 +111,14 @@ class UserProfile extends StatelessWidget {
                             color: Color(0xffFFFFFF),
                           ),
                         ),
-                        Padding(padding: EdgeInsets.fromLTRB(10,0, 0, 0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
                         ),
-                          Icon(Icons.shopping_cart,
-                            size: 25,
-                            color: Color(0xffFFFFFF),
-                          ),
+                        Icon(
+                          Icons.shopping_cart,
+                          size: 25,
+                          color: Color(0xffFFFFFF),
+                        ),
                       ],
                     ),
                     Row(
@@ -137,13 +142,14 @@ class UserProfile extends StatelessWidget {
           Expanded(
             child: RowReuseable(
               row: Row(children: [
-                Icon(Icons.wallet_sharp,
+                Icon(
+                  Icons.wallet_sharp,
                   size: 25,
                   color: Color(0xffFFFFFF),
                 ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  ),
+                ),
                 Text(
                   'To Pay',
                   style: TextStyle(
@@ -157,11 +163,13 @@ class UserProfile extends StatelessWidget {
           Expanded(
             child: RowReuseable(
               row: Row(children: [
-                Icon(Icons.local_shipping,
+                Icon(
+                  Icons.local_shipping,
                   size: 25,
                   color: Color(0XFFFFFFFF),
                 ),
-                Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                 ),
                 Text(
                   'To Ship',
@@ -176,11 +184,13 @@ class UserProfile extends StatelessWidget {
           Expanded(
             child: RowReuseable(
               row: Row(children: [
-                Icon(Icons.archive_outlined,
+                Icon(
+                  Icons.archive_outlined,
                   size: 25,
                   color: Color(0xffFFFFFF),
                 ),
-                Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                 ),
                 Text(
                   'To Receive',
@@ -195,11 +205,13 @@ class UserProfile extends StatelessWidget {
           Expanded(
             child: RowReuseable(
               row: Row(children: [
-                Icon(Icons.star_rounded,
+                Icon(
+                  Icons.star_rounded,
                   size: 30,
                   color: Color(0xffFFFFFF),
                 ),
-                Padding(padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
                 ),
                 Text(
                   'To Rate',
@@ -219,7 +231,7 @@ class UserProfile extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Color(0XFF6D6E4C),
                 borderRadius: BorderRadius.circular(9),
-                ),
+              ),
               child: TextButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/login');
@@ -228,9 +240,9 @@ class UserProfile extends StatelessWidget {
                     "Logout",
                     style: TextStyle(
                       fontFamily: 'OswaldReg',
-                      fontSize:20,
+                      fontSize: 20,
                       color: Color(0xffFFFFFF),
-                      ),
+                    ),
                   )),
               width: 150,
               height: 45,
