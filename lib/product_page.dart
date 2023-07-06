a817055021e8f6030b6bdba38cbd2afd879cb08a
Surya Halim <suryahalim788@gmail.com>
2023-07-06 22:40:01 +0700

    Update product_page.dart

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
+
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product',
+        style: TextStyle(
+          fontFamily: 'OswaldReg',
+        ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Image(
+                  image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/PlayStation_5_and_DualSense_with_transparent_background.png/330px-PlayStation_5_and_DualSense_with_transparent_background.png'),
+                  width: 300,
+                  height: 300,
+                  alignment: Alignment.centerRight,
+                ),
+              ],
+            ),
+          )
+        ]),
+    );
   }
 }
