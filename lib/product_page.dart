326d32e024b03334b23181d4b99bce6b0191efb2
FriesX <edgarchuang343@gmail.com>
2023-07-07 09:07:13 +0700

    Update product_page.dart

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
@@ -8,27 +7,28 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Product',
-        style: TextStyle(
-          fontFamily: 'OswaldReg',
-        ),
+        title: const Text(
+          'Product',
+          style: TextStyle(
+            fontFamily: 'OswaldReg',
+          ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: Row(
-              children: [
-                Image(
-                  image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/PlayStation_5_and_DualSense_with_transparent_background.png/330px-PlayStation_5_and_DualSense_with_transparent_background.png'),
-                  width: 300,
-                  height: 300,
-                  alignment: Alignment.centerRight,
-                ),
-              ],
-            ),
-          )
-        ]),
+      body: Column(children: [
+        Container(
+          child: const Row(
+            children: [
+              Image(
+                image: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/PlayStation_5_and_DualSense_with_transparent_background.png/330px-PlayStation_5_and_DualSense_with_transparent_background.png'),
+                width: 300,
+                height: 300,
+                alignment: Alignment.centerRight,
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
