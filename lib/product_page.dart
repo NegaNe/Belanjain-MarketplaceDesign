f689aceace94f6334a9829ab42f82ae7b5c323c5
NegaNe <manfireburst@gmail.com>
2023-07-08 11:23:46 +0700

    Update product_page.dart

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -14,106 +14,81 @@ class ProductPage extends StatelessWidget {
             ),
           ),
         ),
-
-
-
-
         bottomNavigationBar: BottomAppBar(
-          color: Colors.green,
-          padding: EdgeInsets.all(20),
-          child: Row(
-            children: [
-              const Text(
-                "Rp. 8.999.999",
-                style: TextStyle(fontWeight: FontWeight.bold)
-              ),
-
-              const Expanded(
-                child: SizedBox(width: 100),
+            color: Colors.green,
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                const Text("Rp. 8.999.999",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                const Expanded(
+                  child: SizedBox(width: 100),
                 ),
-              
-
                 Container(
-                width: 100,
-                height: 100,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Row(
-                  children: [
-                    Text(
-                      "Add to Cart",
-                      style: TextStyle(color: Colors.white),
-                      textAlign: TextAlign.center,
-                      )
-                ],)
-              ),
-              
-
-              Container(
-                width: 50,
-                height: 50,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: Row(
-                  children: [
+                    width: 100,
+                    height: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Row(
+                      children: [
+                        Text(
+                          "Add to Cart",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    )),
+                Container(
+                  width: 50,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Row(children: [
                     Icon(
                       Icons.message_sharp,
                       color: Colors.white,
                     ),
-                  ]
-                ),
-              )
-            ],
-          )
+                  ]),
+                )
+              ],
+            )
 
+            // child: Row(
+            //   children: [
+            //     Container(
+            //       alignment: Alignment.centerLeft,
+            //       child: Row(
+            //         children: [
+            //           const Text(
+            //             "Rp. 8.999.999",
+            //             style: TextStyle(fontWeight: FontWeight.bold),
+            //           ),
 
+            //           Card(
+            //             shape: RoundedRectangleBorder(
+            //               borderRadius: BorderRadius.circular(15)
+            //             ),
+            //             color: Colors.black,
+            //             child: const Row(
+            //               children: [
+            //                 Text(
+            //                   "Add to Cart",
+            //                   style: TextStyle(color: Colors.white),
+            //                   )
+            //               ],
+            //             ),
+            //           )
 
-
-
-          // child: Row(
-          //   children: [
-          //     Container(
-          //       alignment: Alignment.centerLeft,
-          //       child: Row(
-          //         children: [
-          //           const Text(
-          //             "Rp. 8.999.999",
-          //             style: TextStyle(fontWeight: FontWeight.bold),
-          //           ),
-
-
-          //           Card(
-          //             shape: RoundedRectangleBorder(
-          //               borderRadius: BorderRadius.circular(15)
-          //             ),
-          //             color: Colors.black,
-          //             child: const Row(
-          //               children: [
-          //                 Text(
-          //                   "Add to Cart",
-          //                   style: TextStyle(color: Colors.white),
-          //                   )
-          //               ],
-          //             ),
-          //           )
-
-
-          //         ],
-          //       )
-          //     )
-          //   ],
-          // )
-        ),
-
-
-
-
+            //         ],
+            //       )
+            //     )
+            //   ],
+            // )
+            ),
         body: Column(
           children: [
             Container(
@@ -152,8 +127,7 @@ class ProductPage extends StatelessWidget {
                   children: [
                     SizedBox(
                         height: 50,
-                        child: Row(
-                          children: [
+                        child: Row(children: [
                           Icon(
                             Icons.star,
                             color: Colors.yellow,
@@ -161,11 +135,7 @@ class ProductPage extends StatelessWidget {
                           ),
                           Text("\t  4.9   "),
                           Text("\t  Rating 120   ")
-                        ]
-                      )
-                    ),
-
-
+                        ])),
                     Row(
                       children: [
                         Icon(
@@ -178,33 +148,36 @@ class ProductPage extends StatelessWidget {
                       ],
                     )
                   ],
-                )
-              ),
+                )),
 
             Container(
               height: 200,
               color: Colors.grey,
-              child: const Column(
+              child: Column(
                 children: [
-                  SizedBox(
-                    child: Row(
-                      children: [
-                        Text(
-                          "Description",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                           )
-                      ],
+                  // const SizedBox(
+                  //   child: Row(
+                  //     children: [
+                  //       Text(
+                  //         "Description",
+                  //         style: TextStyle(fontWeight: FontWeight.bold),
+                  //       )
+                  //     ],
+                  //   ),
+                  // ),
+
+                  Container(
+                    child: Text(
+                      'Description',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
-
-                  SizedBox(
-                    child: Row(
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    child: const Column(
                       children: [
                         Text(
                           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                          maxLines: 7,
-                          textDirection: TextDirection.rtl,
-                          textAlign: TextAlign.justify,
                         )
                       ],
                     ),
