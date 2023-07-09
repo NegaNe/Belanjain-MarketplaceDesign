14d236eeec829b26f54a4df736361295dffeb011
FriesX <edgarchuang343@gmail.com>
2023-07-10 00:25:07 +0700

    update

--- lib/payment.dart
+++ lib/payment.dart
@@ -29,7 +29,7 @@ class Payment extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsetsDirectional.all(25),
+            margin: const EdgeInsetsDirectional.all(25),
             // color: Colors.amber,
             child: const Row(
               children: [
@@ -50,14 +50,14 @@ class Payment extends StatelessWidget {
           Column(
             children: [
               Container(
-                margin: EdgeInsets.fromLTRB(25, 0, 25, 0),
-                padding: EdgeInsets.fromLTRB(0, 25, 0, 25),
+                margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
                 decoration: BoxDecoration(
-                  color: Color(0xffFBFBFB),
-                    border: Border.all(
-                        width: 1, color: Color.fromARGB(255, 163, 163, 163)),
-                    // color: Color.fromARGB(50, 251, 251, 251)
-                    ),
+                  color: const Color(0xffFBFBFB),
+                  border: Border.all(
+                      width: 1, color: Color.fromARGB(255, 163, 163, 163)),
+                  // color: Color.fromARGB(50, 251, 251, 251)
+                ),
                 child: const Column(
                   children: [
                     Row(
@@ -90,29 +90,27 @@ class Payment extends StatelessWidget {
               )
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
           DecoratedBox(
-              decoration: BoxDecoration(
-                color: Color(0xffF1FFC4),
+              decoration: const BoxDecoration(
+                  color: Color(0xffF1FFC4),
                   // color: const Color.fromRGBO(186, 219, 82, 82),
                   border: Border(
                     top: BorderSide(color: Color(0xffBADB52)),
                     bottom: BorderSide(color: Color(0xffBADB52)),
-                  )
-                  ),
+                  )),
               child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.fromLTRB(22, 10, 0, 10),
+                    padding: const EdgeInsets.fromLTRB(22, 10, 0, 10),
                     child: const Column(
                       children: [
                         Row(children: [
-                          Text("Shipping Option",
-                          style: TextStyle(
-                            fontFamily: 'OswaldReg'
-                          ),
+                          Text(
+                            "Shipping Option",
+                            style: TextStyle(fontFamily: 'OswaldReg'),
                           ),
                         ]),
                         Row(
@@ -141,82 +139,82 @@ class Payment extends StatelessWidget {
                   ),
                 ],
               )),
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
           DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border(
-                    top: BorderSide(color: Color(0xff9B9B9B)),
-                    bottom: BorderSide(color: Color(0xff9B9B9B)),
-                  ),
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Color(0xff9B9B9B)),
+                bottom: BorderSide(color: Color(0xff9B9B9B)),
+              ),
             ),
             child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 15),
+                margin: const EdgeInsets.symmetric(horizontal: 15),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
+                    const Text(
                       "Use Voucher",
                       style: TextStyle(fontSize: 24),
                     ),
                     Container(
                       child: Text('Enter Voucher >',
-                      style: TextStyle(
-                        color: Color(0xff9B9B9B),
-                      )
-                      ),
+                          style: const TextStyle(
+                            color: Color(0xff9B9B9B),
+                          )),
                       alignment: Alignment.centerRight,
                     )
                   ],
                 )),
           ),
-          SizedBox(
+          const SizedBox(
             height: 55,
           ),
           DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border(
-                    top: BorderSide(color: Color(0xff9B9B9B)),
-                    bottom: BorderSide(color: Color(0xff9B9B9B)),
-                  ),
-                  ),
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Color(0xff9B9B9B)),
+                bottom: BorderSide(color: Color(0xff9B9B9B)),
+              ),
+            ),
             child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 15),
+                margin: const EdgeInsets.symmetric(horizontal: 15),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
+                    const Text(
                       "Payment Options",
                       style: TextStyle(fontSize: 24),
                     ),
                     Container(
-                      child: Text('COD >',
-                      style: TextStyle(
-                        color: Color(0xff9B9B9B),
-                      ),
+                      child: const Text(
+                        'COD >',
+                        style: TextStyle(
+                          color: Color(0xff9B9B9B),
+                        ),
                       ),
                       alignment: Alignment.centerRight,
                     )
                   ],
                 )),
           ),
-          SizedBox(
+          const SizedBox(
             height: 25,
           ),
           Container(
-              margin: EdgeInsets.only(left: 130),
+              margin: const EdgeInsets.only(left: 130),
               child: TextButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/discover');
                   },
                   child: Container(
-                    margin: EdgeInsets.only(left: 0),
-                    padding: EdgeInsets.fromLTRB(90, 10, 90, 10),
+                    margin: const EdgeInsets.only(left: 0),
+                    padding: const EdgeInsets.fromLTRB(90, 10, 90, 10),
                     decoration: BoxDecoration(
-                        color: Color.fromRGBO(31, 142, 245, 1),
+                        color: const Color.fromRGBO(31, 142, 245, 1),
                         borderRadius: BorderRadius.circular(10)),
-                    child: Text(
+                    child: const Text(
                       'PAY',
                       style: TextStyle(fontSize: 24, color: Colors.black),
                     ),
